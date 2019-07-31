973703ec10faf20046cd1e5201585acdf9670565
michele <michele.bonapace@studenti.unitn.it>
2019-07-31 16:21:07 +0200

    added multiple coroutine and example

---
+++ Assets/Example/Example.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Mich.ConsecutiveCoroutine
+{
+    public class Example : MonoBehaviour
+    {
+        public Text text;
+        // Start is called before the first frame update
+        void Start()
+        {
+            //one cycle only
+            ConsecutiveCoroutineNormal();
+
+            //loop
+            //StartCoroutine(ConsecutiveCoroutineLoop());
+        }
+
+
+        private void ConsecutiveCoroutineNormal()
+        {
+            ConsecutiveCoroutine cc = new ConsecutiveCoroutine(this);
+            //USE DELEGATE TO ADD A FUNCTION
+            cc.Add(delegate { ChangeTextColorInstant(Color.blue); });
+            //NOTHING TO ADD AN IENUMERATOR
+            cc.Add(SetTextAndWait("1"));
+            cc.Add(SetTextColorAndWait(Color.red));
+            cc.Add(SetTextAndWait("2"));
+            cc.Add(SetTextColorAndWait(Color.blue));
+            cc.Invoke();
+        }
+
+
+        private IEnumerator ConsecutiveCoroutineLoop()
+        {
+            while (true)
+            {
+                ConsecutiveCoroutine cc = new ConsecutiveCoroutine(this);
+                //USE DELEGATE TO ADD A FUNCTION
+                cc.Add(delegate { ChangeTextColorInstant(Color.blue); });
+                //NOTHING TO ADD AN IENUMERATOR
+                cc.Add(SetTextAndWait("1"));
+                cc.Add(SetTextAndWait("2"));
+                cc.Add(SetTextColorAndWait(Color.blue));
+                yield return cc.InvokeLoop();
+            }
+        }
+
+        private void ChangeTextColorInstant(Color c)
+        {
+            text.color = c;
+        }
+
+        private IEnumerator SetTextAndWait(string s)
+        {
+            text.text = s;
+            yield return new WaitForSeconds(1);
+        }
+
+        private IEnumerator SetTextColorAndWait(Color c)
+        {
+            text.color = Color.red;
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ConsecutiveCoroutine.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Mich.ConsecutiveCoroutine
+{
+    enum ActionType {COROUTINE, FUNCTION};
+
+    [System.Serializable]
+    class Couple
+    {
+        public object action;
+        public ActionType type;
+        public Couple(object action, ActionType type){
+            this.action = action;
+            this.type = type;
+        }
+    }
+
+
+
+    public class ConsecutiveCoroutine
+    {
+        private Queue<Couple> actions = new Queue<Couple>();
+        private MonoBehaviour invoker;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="invoker">The object on which the coroutine will be called, ususally "this"</param>
+        /// <param name="cyclic"></param>
+        public ConsecutiveCoroutine(MonoBehaviour invoker)
+        {
+            this.invoker = invoker;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="invoker">The object on which the coroutine will be called, ususally "this"</param>
+        /// <param name="cyclic"></param>
+        public ConsecutiveCoroutine(MonoBehaviour invoker, IEnumerator[] actions)
+        {
+            this.invoker = invoker;
+            foreach (IEnumerator a in actions)
+            {
+                Add(a);
+            }
+        }
+
+        /// <summary>
+        /// Add a coroutine at the end of the queue
+        /// </summary>
+        /// <param name="action"></param>
+        public void Add(IEnumerator action)
+        {
+            actions.Enqueue(new Couple(action, ActionType.COROUTINE));
+        }
+
+        /// <summary>
+        /// Add a function at the end of the queue, use "delegate {function();"}
+        /// </summary>
+        /// <param name="action"></param>
+        public void Add(UnityAction action)
+        {
+            actions.Enqueue(new Couple(action, ActionType.FUNCTION));
+        }
+
+        /// <summary>
+        /// Starts the coroutines one after the other
+        /// </summary>
+        public void Invoke()
+        {
+            invoker.StartCoroutine(Begin());
+        }
+
+        public IEnumerator InvokeLoop()
+        {
+            yield return invoker.StartCoroutine(Begin());
+        }
+
+        private IEnumerator Begin()
+        {
+            while (actions.Count > 0)
+            {
+                Couple couple = actions.Dequeue();
+                if (couple.type == ActionType.FUNCTION)
+                {
+                    ((UnityAction)couple.action).Invoke();
+                }
+                else if(couple.type == ActionType.COROUTINE)
+                {
+                    yield return invoker.StartCoroutine((IEnumerator)couple.action);
+                }
+            }
+        }
+    }
+}
